dcb781ce1c049fa202ac7e767f0feda9cead5f89
Panos <optas@stanford.edu>
2016-12-21 20:18:17 -0800

    considerations

---
+++ private/consider.py
@@ -0,0 +1,21 @@
+'''
+Created on December 21, 2016.
+Keep things that you will consider to include in main library. 
+
+@author:    Panas Achlioptas
+@contact:   pachlioptas @ gmail.com
+@copyright: You are free to use, change, or redistribute this code in any
+            way you want for non-commercial purposes. 
+'''
+# Alternative to plotting a mesh with matplotlib.
+# Need to find way to colorize vertex/faces.
+# from mpl_toolkits.mplot3d import Axes3D
+# from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+# fig = plt.figure()
+# ax = Axes3D(fig)
+# v = in_mesh.vertices
+# for t in in_mesh.triangles:
+#     tri = Poly3DCollection([v[t]])
+#     tri.set_color(colors.rgb2hex([0,1,0]))
+#     ax.add_collection3d(tri)
+# # # # plt.show()
