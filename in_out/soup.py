964cc7dd843e2e263f996b259edc2aab7a35de89
Panos <optas@stanford.edu>
2017-01-17 15:06:18 -0800

    mdb

--- in_out/soup.py
+++ in_out/soup.py
@@ -290,17 +290,6 @@ def read_pixel_list_from_txt(in_file):
     y_coord = pixels[:, 1]
     return x_coord, y_coord
 
-def files_in_subdirs(top_dir, search_pattern):
-    res = []    
-    join = os.path.join
-    regex = re.compile(search_pattern)
-    for path, _, files in os.walk(top_dir):
-        for name in files:
-            full_name = join(path, name)
-            if regex.search(full_name):
-                res.append(full_name)
-    return res
-
 # ADD as way of making mask from image.  
 # image         = cv2.imread(views_folder + '/img-00-00.png', cv2.IMREAD_UNCHANGED)
 # plt.subplot(1,2,1); plt.imshow(image)
